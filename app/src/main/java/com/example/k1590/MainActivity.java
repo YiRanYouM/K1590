61007d320580d04bf41ac8e1939389445879dd58
YiRanYouM <623871843@qq.com>
2020-11-17 23:41:30 +0800

    11.17 commit

--- app/src/main/java/com/example/k1590/MainActivity.java
+++ app/src/main/java/com/example/k1590/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.SharedPreferences;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,8 +22,21 @@ import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button bt_light, bt_temp;
@@ -34,6 +48,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String user_name;
     private ImageView iv_setting;
     private List<ThingBean> thingList = new ArrayList<>();
+
+    //mqtt
+    private final String TAG = "AiotMqtt";
+    /* 设备三元组信息 */
+    final private String PRODUCTKEY = "a11PkRH5FFV";
+    final private String DEVICENAME = "Android";
+    final private String DEVICESECRET = "c7fe2d3a05bc21a7ec661705f9b627eb";
+
+    /* 自动Topic, 用于上报消息 */
+    final private String PUB_TOPIC = "/" + PRODUCTKEY + "/" + DEVICENAME + "/user/update";
+    /* 自动Topic, 用于接受消息 */
+    final private String SUB_TOPIC = "/" + PRODUCTKEY + "/" + DEVICENAME + "/user/get";
+
+    /* 阿里云Mqtt服务器域名 */
+    final String host = "tcp://" + PRODUCTKEY + ".iot-as-mqtt.cn-shanghai.aliyuncs.com:443";
+    private String clientId;
+    private String userName;
+    private String passWord;
+
+    MqttAndroidClient mqttAndroidClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +93,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         user_name = sp.getString("name", null);
 
+        //mqtt
+        /* 获取Mqtt建连信息clientId, username, password */
+        AiotMqttOption aiotMqttOption = new AiotMqttOption().getMqttOption(PRODUCTKEY, DEVICENAME, DEVICESECRET);
+        if (aiotMqttOption == null) {
+            Log.e(TAG, "device info error");
+        } else {
+            clientId = aiotMqttOption.getClientId();
+            userName = aiotMqttOption.getUsername();
+            passWord = aiotMqttOption.getPassword();
+        }
+
+        /* 创建MqttConnectOptions对象并配置username和password */
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setUserName(userName);
+        mqttConnectOptions.setPassword(passWord.toCharArray());
+
+
+        /* 创建MqttAndroidClient对象, 并设置回调接口 */
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), host, clientId);
+        mqttAndroidClient.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.i(TAG, "connection lost");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.i(TAG, "topic: " + topic + ", msg: " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                Log.i(TAG, "msg delivered");
+            }
+        });
+
+        /* Mqtt建连 */
+        try {
+            mqttAndroidClient.connect(mqttConnectOptions,null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.i(TAG, "connect succeed");
+
+                    subscribeTopic(SUB_TOPIC);
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.i(TAG, "connect failed");
+                }
+            });
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -82,7 +171,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .setGridItemClickListener(new GridPager.GridItemClickListener() {
                     @Override
                     public void click(int position) {
+                        if (thingList.get(position).getType().equals("0")) {
+                            publishMessage("{'temperature':'/i/'}");
+                        }else if (thingList.get(position).getType().equals("1")){
 
+                        }
                     }
                 })
                 .show();
@@ -158,7 +251,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.bt_temp:
                 String temp_order = sp.getString("temp_order", null);
-                showDialog(this, "当前温度","");
+                showDialog(this, "当前温度","/i/");
                 break;
             case R.id.iv_setting:
                 menuPop();
@@ -205,6 +298,111 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 dialog.dismiss();
             }
         });
+    }
+
+    /**
+     * 订阅特定的主题
+     * @param topic mqtt主题
+     */
+    public void subscribeTopic(String topic) {
+        try {
+            mqttAndroidClient.subscribe(topic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.i(TAG, "subscribed succeed");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.i(TAG, "subscribed failed");
+                }
+            });
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 向默认的主题/user/update发布消息
+     * @param payload 消息载荷
+     */
+    public void publishMessage(String payload) {
+        try {
+            if (mqttAndroidClient.isConnected() == false) {
+                mqttAndroidClient.connect();
+            }
+
+            MqttMessage message = new MqttMessage();
+            message.setPayload(payload.getBytes());
+            message.setQos(0);
+            mqttAndroidClient.publish(PUB_TOPIC, message,null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.i(TAG, "publish succeed!");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.i(TAG, "publish failed!");
+                }
+            });
+        } catch (MqttException e) {
+            Log.e(TAG, e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * MQTT建连选项类，输入设备三元组productKey, deviceName和deviceSecret, 生成Mqtt建连参数clientId，username和password.
+     */
+    class AiotMqttOption {
+        private String username = "";
+        private String password = "";
+        private String clientId = "";
+
+        public String getUsername() { return this.username;}
+        public String getPassword() { return this.password;}
+        public String getClientId() { return this.clientId;}
+
+        /**
+         * 获取Mqtt建连选项对象
+         * @param productKey 产品秘钥
+         * @param deviceName 设备名称
+         * @param deviceSecret 设备机密
+         * @return AiotMqttOption对象或者NULL
+         */
+        public AiotMqttOption getMqttOption(String productKey, String deviceName, String deviceSecret) {
+            if (productKey == null || deviceName == null || deviceSecret == null) {
+                return null;
+            }
+
+            try {
+                String timestamp = Long.toString(System.currentTimeMillis());
+
+                // clientId
+                this.clientId = productKey + "." + deviceName + "|timestamp=" + timestamp +
+                        ",_v=paho-android-1.0.0,securemode=2,signmethod=hmacsha256|";
+
+                // userName
+                this.username = deviceName + "&" + productKey;
+
+                // password
+                String macSrc = "clientId" + productKey + "." + deviceName + "deviceName" +
+                        deviceName + "productKey" + productKey + "timestamp" + timestamp;
+                String algorithm = "HmacSHA256";
+                Mac mac = Mac.getInstance(algorithm);
+                SecretKeySpec secretKeySpec = new SecretKeySpec(deviceSecret.getBytes(), algorithm);
+                mac.init(secretKeySpec);
+                byte[] macRes = mac.doFinal(macSrc.getBytes());
+                password = String.format("%064x", new BigInteger(1, macRes));
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            return this;
+        }
     }
 
     @Override
